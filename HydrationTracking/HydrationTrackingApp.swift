228281b355617655a14b93bdf09ecccf78def8e6
Monaalruthaya <monaalruthaya@outlook.com>
2025-05-31 19:58:08 +0300

    Initial Commit

---
+++ HydrationTracking/HydrationTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationTrackingApp.swift
+//  HydrationTracking
+//
+//  Created by mona alruthaya on 04/12/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HydrationTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HydrationTrackingTests/HydrationTrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationTrackingTests.swift
+//  HydrationTrackingTests
+//
+//  Created by mona alruthaya on 04/12/1446 AH.
+//
+
+import Testing
+@testable import HydrationTracking
+
+struct HydrationTrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
